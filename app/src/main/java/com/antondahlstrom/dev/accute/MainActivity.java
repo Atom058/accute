979ba0eb5f8c7417c6a7b4e9c9937d6a7987c9c4
atom058 <anton.dahlstrom508@gmail.com>
2015-02-14 00:19:38 +0100

    Stable initial app with Parse operational

--- app/src/main/java/com/antondahlstrom/dev/accute/MainActivity.java
+++ app/src/main/java/com/antondahlstrom/dev/accute/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "elcRDDFIFh8rrIPwM48qn1MJGSEbZQLVFUM3pn3W", "TJk6sCh9sWQEU0HCKK03K9fLRvnwEFDa8");
+
     }
 
 
